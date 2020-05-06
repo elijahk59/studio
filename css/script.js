f0f9d21d10fb7d4132d9243e81bd54cc2212391c
elijak59 <elijahk59@gmail.com>
2020-05-06 19:58:35 +0300

    shortened my javascript code

--- css/script.js
+++ css/script.js
@@ -1,12 +1,14 @@
-jQuery('h1').click(function(){
-    alert("This is a heading tag")
-  });
-
-jQuery('p').click(function(){
-    alert("This is a paragraph")
-  });
-
-jQuery('img').click(function(){
-    alert("This is an image")
+$(document).ready(function() {
+    $("h1").click(function() {
+      alert("This is a header.");
+    });
+  
+    $("p").click(function() {
+      alert("This is a paragraph.");
+    });
+  
+    $("img").click(function() {
+      alert("This is an image.");
+    });
   });
   
